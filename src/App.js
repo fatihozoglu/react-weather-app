2e53e02a6982355fd4d241a6edad0536679817e1
Fatih <68196068+fatihozoglu@users.noreply.github.com>
2021-10-11 13:09:31 +0300

    Create .env file

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import CurrentForecast from "./components/CurrentForecast";
 import FutureForecast from "./components/FutureForecast";
 import { useState, useEffect } from "react";
 
-const API_KEY = "ecabe5ca1bf326def30ca32b97198ad9";
-
 function App() {
   const [city, setCity] = useState("Ankara");
   const [tempType, setTempType] = useState("°C");
@@ -17,11 +15,11 @@ function App() {
     let lon;
     async function fetchLocation() {
       try {
-        let location = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${API_KEY}`);
+        let location = await fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=1&appid=${process.env.REACT_APP_API_KEY}`);
         location = await location.json();
         lat = await location[0].lat;
         lon = await location[0].lon;
-        let info = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=${unit}&exclude=minutely,hourly,alerts&appid=${API_KEY}`);
+        let info = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=${unit}&exclude=minutely,hourly,alerts&appid=${process.env.REACT_APP_API_KEY}`);
         info = await info.json();
         setData(info);
       }
